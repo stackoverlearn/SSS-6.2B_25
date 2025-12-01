25f4bed8bf1319d02e4b1a841fcf42c679428532
James Attard <james.a.attard@mcast.edu.mt>
2025-12-01 11:25:13 +0000

    Integrated layout to the index view

--- contactApp/resources/views/contacts/index.blade.php
+++ contactApp/resources/views/contacts/index.blade.php
@@ -1,5 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
 <h1>All Contacts</h1>
 <div>
     <li><a href="{{ route('contacts.create') }}">Add Contact</a></li>
     <li><a href="{{ route('contacts.show', 1) }}">Show a Contact</a></li>
 </div>
+@endsection
