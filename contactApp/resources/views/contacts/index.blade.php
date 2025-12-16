11b8838d93e83b78681aab04ec5204325977ddd2
James Attard <james.a.attard@mcast.edu.mt>
2025-12-16 13:00:02 +0000

    Minor changes

--- contactApp/resources/views/contacts/index.blade.php
+++ contactApp/resources/views/contacts/index.blade.php
@@ -58,7 +58,7 @@
                           <td>{{ $contact->email }}</td>
                           <td>{{ $contact->company->name }}</td>
                           <td width="150">
-                            {{-- <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a> --}}
+                            <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
                             <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
                             <a href="{{ route('contacts.destroy', $contact->id) }}" class="btn-delete btn btn-sm btn-circle btn-outline-danger" title="Delete"><i class="fa fa-times"></i></a>
                           </td>
