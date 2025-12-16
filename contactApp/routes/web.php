72d51abd0389706c10357c351489ecd70c5f6673
James Attard <james.a.attard@mcast.edu.mt>
2025-12-16 13:14:43 +0000

    Fixed show/edit route typo

--- contactApp/routes/web.php
+++ contactApp/routes/web.php
@@ -22,6 +22,6 @@ Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.ind
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
 Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts/{id}', [ContactController::class, 'edit'])->name('contacts.edit');
+Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
 Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
 Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
